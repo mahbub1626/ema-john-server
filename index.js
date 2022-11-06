ce18503c1eff0bd4746cd3e60c25009e21858fd5
mahbub1626 <mahbub1626@gmail.com>
2022-11-06 23:14:24 +0600

    pagonation now working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -12,7 +12,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3sh2wxl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -20,8 +19,23 @@ async function run() {
     try {
         const productsCollection = client.db('emaJohn').collection('products');
 
-        app.get('/products', async(req,res)=>{
-            const query ={};
+        app.get('/products', async (req, res) => {
+            // pagination
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size)
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.skip(page * size).limit(size).toArray();
+            const count = await productsCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        });
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            console.log(ids)
+            const objectsIds = ids.map(id => ObjectId(id));
+            const query = {_id: {$in: objectsIds}};
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
